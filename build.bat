ebc772b7d4305c78805fd79ee05444e202eb5455
Egor Borisovsky <mr-warete@mail.ru>
2021-12-26 16:38:55 +0300

    изменил скрипт сборки

--- build.bat
+++ build.bat
@@ -1,8 +1,9 @@
 :: Нужно в texworks добавить новый инструмент верстки, указав текущий файл
 :: назвать biber+pdflatex+bibtex
 :: передавать скрипту параметры в таком же порядке как указаны ниже
-:: $basename $fullname
+:: $basename $fullname $synctexoption
 @echo off
 echo %1
 echo %2
-Start biber.exe %1 && texify.exe --pdf --synctex=1 --clean %2
+
+Start miktex-pdftex.exe -synctex=1 -undump=pdflatex %2 && biber.exe %1 && miktex-pdftex.exe %3 -undump=pdflatex %2
